1cd80434d14657718dc5e6d815d1ff829c698185
Diego Leite Asprino <diegoasprino07@gmail.com>
2025-06-04 17:59:39 -0300

    quiz feito

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 const quiz = document.getElementById("quiz-form")
+const nota_id = document.getElementById("nota")
+const resultado = document.getElementById("resultado")
 let nota = 0
 let respostas = []
 let respostas_certas = ["1B","2C","3D","4B","5B","6C","7C","8B","9C","10C"]
@@ -7,7 +9,6 @@ quiz.addEventListener(("submit"), (e) => {
     e.preventDefault();
     nota = 0
     
-
     for(i = 0; i < 40; i++){
         if(quiz[i].checked == true){
             respostas.push(quiz[i].id)
@@ -20,7 +21,12 @@ quiz.addEventListener(("submit"), (e) => {
         }
     }
 
+
     localStorage.setItem("nota-quiz",nota)
-    console.log(localStorage.getItem("nota-quiz"))
-    window.location.href = "/pages/nota.html"
+    lançar_nota()
 })
+
+function lançar_nota(){ 
+    resultado.style.display = "block"
+    nota_id.textContent = localStorage.getItem("nota-quiz")
+}
